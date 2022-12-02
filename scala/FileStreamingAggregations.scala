ecee822aa458d4ab0a25eb399875028335a63f1e
Vasu Bajaj <vbajaj@melissaanddoug.com>
2022-12-02 20:07:10 +0530

    add scala code

---
+++ scala/FileStreamingAggregations.scala
@@ -0,0 +1,71 @@
+package au.com.thoughtworks.assessment.spark.streaming
+
+import java.sql.Timestamp
+
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.{DataFrame, Dataset, SparkSession}
+
+object FileStreamingAggregations {
+
+  //convert aggregates into typed data
+  case class StockEvent(stockName: String, tradeType: String, price: Option[Double], quantity: Option[Int], timestamp: Timestamp, eventTimeReadable: String)
+  case class AggretatedStockEvent(stockName: String, tradeType: String, maxPrice: Option[Double], avgQuantity: Option[Double])
+
+
+  def main(args: Array[String]): Unit = {
+
+    //create a spark session, and run it on local mode
+    val spark = SparkSession.builder()
+      .appName("StreaminAggregations")
+      .master("local[*]")
+      .getOrCreate()
+
+    //spark.sparkContext.setLogLevel("WARN")
+
+    import spark.implicits._
+
+    //define the schema
+    val schema = StructType(
+      StructField("stockName", StringType) ::
+        StructField("tradeType", StringType) ::
+        StructField("price", DoubleType) ::
+        StructField("quantity", IntegerType) ::
+        StructField("timestamp", LongType) ::
+        StructField("eventTimeReadable", StringType) :: Nil)
+
+    //read the files from source
+    val stocks: DataFrame = spark
+      .readStream
+      .schema(schema)
+      .csv("/Users/vasu/workspace/stock_stream_processing/src/test/resources/testdata/")
+
+    //do aggregates
+    val aggregates = stocks
+      .groupBy("stockName", "tradeType")
+      .agg(
+        "price" → "max",
+        "quantity" → "avg")
+      .withColumnRenamed("max(price)", "maxPrice")
+      .withColumnRenamed("avg(quantity)", "avgQuantity")
+
+    aggregates.printSchema()
+    aggregates.explain()
+
+    val typedAggregates = aggregates.as[AggretatedStockEvent]
+    val filtered  = typedAggregates
+      .filter(_.maxPrice.exists(_ > 70))
+      .where("avgQuantity > 10")
+      .repartition(10)
+
+    val query = filtered
+      .writeStream
+      .queryName("stocks_averages")
+      .partitionBy("stockName")
+      .outputMode("complete")
+      .format("console")
+      .start()
+
+    query.awaitTermination()
+  }
+}
---
+++ scala/HelloStructredStreaming.scala
@@ -0,0 +1,35 @@
+import org.apache.spark.sql.{Dataset, SparkSession}
+
+object HelloStructredStreaming {
+
+  def main(args: Array[String]): Unit = {
+
+    //create a spark session, and run it on local mode
+    val spark = SparkSession.builder()
+      .appName("HelloStructuredStreaming")
+      .master("local[*]")
+      .getOrCreate()
+
+    import spark.implicits._
+
+    //read from a directory as text stream
+    val readme: Dataset[String] = spark
+      .readStream
+      .textFile("/tmp/spark_file_stream/")
+
+    //do word count
+    val words = readme.flatMap(_.split(" "))
+    val wordCounts = words.groupBy("value").count()
+
+    //run the wordCount query and write to console
+    val query = wordCounts
+        .writeStream
+        .queryName("WordCount")
+        .outputMode("complete")
+        .format("console")
+        .start()
+
+    //wait till query.stop() is called
+    query.awaitTermination()
+  }
+}
